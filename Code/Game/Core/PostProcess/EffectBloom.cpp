39b17a9775a08157e3ebf57eecd87414e799085a
CaIzii <2470159730@qq.com>
2025-07-19 02:03:41 -0500

    Add `EffectBloom` class to support bloom post-processing effect and update project files accordingly

---
+++ Code/Game/Core/PostProcess/EffectBloom.cpp
@@ -0,0 +1,20 @@
+﻿#include "EffectBloom.hpp"
+
+#include "Engine/Renderer/IRenderer.hpp"
+
+EffectBloom::EffectBloom(const std::string& name, int priority) : PostProcessEffect(name, priority)
+{
+}
+
+void EffectBloom::Initialize(IRenderer& renderer)
+{
+}
+
+void EffectBloom::Shutdown()
+{
+}
+
+void EffectBloom::Process(RenderTarget* input, RenderTarget* output)
+{
+}
+
---
+++ Code/Game/Core/PostProcess/EffectBloom.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "PostProcessEffect.hpp"
+
+class EffectBloom : public PostProcessEffect
+{
+public:
+    EffectBloom(const std::string& name, int priority);
+
+    void Initialize(IRenderer& renderer) override;
+    void Shutdown() override;
+    
+    void Process(RenderTarget* input, RenderTarget* output) override;
+
+protected:
+
+
+protected:
+
+
+private:
+    
+};
